7b54e9405064c33754d1cd4678558f2269027834
GRETROZH\gretr <jose.gre1222@gmail.com>
2023-06-16 00:11:31 -0500

    se sube la base del microservicio de productos

---
+++ servicio-productos/src/test/java/com/curso/servicio/productos/ServicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.servicio.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
