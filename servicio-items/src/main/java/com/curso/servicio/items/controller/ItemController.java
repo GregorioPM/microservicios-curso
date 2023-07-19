fa34314744f9ce5098c1ce8f219aae0a56ad411d
GRETROZH\gretr <jose.gre1222@gmail.com>
2023-07-19 01:02:02 -0500

    feat: se implementa la alternativa de manejar los errores con las anotaciones

--- servicio-items/src/main/java/com/curso/servicio/items/controller/ItemController.java
+++ servicio-items/src/main/java/com/curso/servicio/items/controller/ItemController.java
@@ -3,6 +3,8 @@ package com.curso.servicio.items.controller;
 import com.curso.servicio.items.models.Item;
 import com.curso.servicio.items.models.Producto;
 import com.curso.servicio.items.service.ItemService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,6 +12,7 @@ import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @AllArgsConstructor
@@ -38,6 +41,20 @@ public class ItemController {
                         e -> metodoAlternativo(id, cantidad, e));
     }
 
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+    @GetMapping("/ver2/{id}/{cantidad}")
+    public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad){
+        return service.findById(id, cantidad);
+    }
+
+    //Solo timelimiter No aplica el estado open en circuit breaker
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+    @TimeLimiter(name = "items")
+    @GetMapping("/ver3/{id}/{cantidad}")
+    public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad){
+        return CompletableFuture.supplyAsync(() -> service.findById(id, cantidad));
+    }
+
     public Item metodoAlternativo(Long id, Integer cantidad, Throwable e){
         log.info("Error: " + e.getMessage());
         Item item = new Item();
@@ -48,6 +65,18 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return item;
+    }
+
+    public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e){
+        log.info("Error: " + e.getMessage());
+        Item item = new Item();
+        Producto producto = new Producto();
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return CompletableFuture.supplyAsync(() -> item);
     }
 
 }
