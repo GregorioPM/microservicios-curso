ebef70d1d1cb58de81b8584b1a547eb322416af8
GRETROZH\gretr <jose.gre1222@gmail.com>
2023-07-26 02:53:47 -0500

    feat: implementado configuracion de config server en items

--- servicio-items/src/main/java/com/curso/servicio/items/controller/ItemController.java
+++ servicio-items/src/main/java/com/curso/servicio/items/controller/ItemController.java
@@ -6,23 +6,34 @@ import com.curso.servicio.items.service.ItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Log4j2
 public class ItemController {
 
+    private final Environment env;
     @Qualifier("serviceFeign")
     private final ItemService service;
 
     private final CircuitBreakerFactory cbFactory;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
     @GetMapping("/listar")
     public List<Item> listar(@RequestParam(name = "nombre", required = false) String nombre,
@@ -79,4 +90,20 @@ public class ItemController {
         return CompletableFuture.supplyAsync(() -> item);
     }
 
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+
+        log.info(texto);
+
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", texto);
+        json.put("puerto", puerto);
+
+        if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+            json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+            json.put("autor.email", env.getProperty("configuracion.autor.email"));
+        }
+
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+    }
 }
